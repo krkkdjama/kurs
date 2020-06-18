c8232a63cd288e6290f330ddebade43435721239
krkk <krkkdjama@gmail.com>
2020-06-18 20:17:49 +0300

    fix reset form

--- dz 15.06.2020/js/jquery-form.js
+++ dz 15.06.2020/js/jquery-form.js
@@ -7,6 +7,7 @@ $("form").on("submit", function(e) {
 $.post("http://109.235.69.203:8080/data/1001/", {
 name, date, car, exam
 });
+$("form").trigger('reset');
 $(`button[name=result]`).on("click", function(e) {
     $("table").append(`<tr> <td>${name}</td> <td>${date}</td> <td>${car}</td> <td>${exam}</td> </tr>`)
 });
