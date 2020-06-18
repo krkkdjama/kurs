11f98c7278ab729fdc12b665683e8022d26bceb0
krkk <krkkdjama@gmail.com>
2020-06-18 19:59:41 +0300

    dz 15.06

---
+++ dz 15.06.2020/js/jquery-form.js
@@ -0,0 +1,15 @@
+$("form").on("submit", function(e) {
+    e.preventDefault();
+    var name = $("[name=name]").val();
+    var date = $("[name=date]").val();
+    var car = $("[name=carMake]").val();
+    var exam = $("[name=passExam]").val();
+$.post("http://109.235.69.203:8080/data/1001/", {
+name, date, car, exam
+});
+$(`button[name=result]`).on("click", function(e) {
+    $("table").append(`<tr> <td>${name}</td> <td>${date}</td> <td>${car}</td> <td>${exam}</td> </tr>`)
+});
+});
+
+
